4f30d0837922941fa025f4dc3ccba08cabe21e25
dr.Edelon <denys.pankrushev@gmail.com>
2024-02-18 16:46:04 +0000

    move to main branch

--- main_app/src/main.cpp
+++ main_app/src/main.cpp
@@ -16,23 +16,23 @@ int main() {
     // busScan();
     // dumpAddr(0x40);
     // drawWindow();
-    // testLegs();
+    testLegs();
 
-    int fd = open("/dev/hidraw0", O_RDONLY);
-    if (fd < 0) {
-        std::cerr << "Не удалось открыть устройство" << std::endl;
-        return 1;
-    }
+    // int fd = open("/dev/hidraw0", O_RDONLY);
+    // if (fd < 0) {
+    //     std::cerr << "Не удалось открыть устройство" << std::endl;
+    //     return 1;
+    // }
 
-    struct hidraw_devinfo info;
-    if (ioctl(fd, HIDIOCGRAWINFO, &info) < 0) {
-        std::cerr << "Ошибка при получении информации о дескрипторе устройства" << std::endl;
-        close(fd);
-        return 1;
-    }
+    // struct hidraw_devinfo info;
+    // if (ioctl(fd, HIDIOCGRAWINFO, &info) < 0) {
+    //     std::cerr << "Ошибка при получении информации о дескрипторе устройства" << std::endl;
+    //     close(fd);
+    //     return 1;
+    // }
 
-    std::cout << "Вендор: " << info.vendor << " Продукт: " << info.product << std::endl;
+    // std::cout << "Вендор: " << info.vendor << " Продукт: " << info.product << std::endl;
 
-    close(fd);
+    // close(fd);
     return 0;
 }
