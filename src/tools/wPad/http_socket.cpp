3d1cb633de3c92c437511a3c53ecdeb84d8e1cac
dr.Edelon <Densy.Pankrushev@gmail.com>
2025-06-10 00:03:45 +0100

    trigger fix

--- src/tools/wPad/http_socket.cpp
+++ src/tools/wPad/http_socket.cpp
@@ -11,7 +11,7 @@
 #include "terminal_tools.h"   // WARNING/LOG
 
 #define NL  "\r\n"
-#define BUF_SIZE 4096
+#define BUF_SIZE 8192
 #define HTTP_PL(name) name, sizeof(name) - 1
 
 #pragma pack(push, 1)
