6c799f23c058687e21bdb1f9158f3173067c150d
dr.Edelon <Denys.Pankrushev@gmail.com>
2026-08-14 05:31:25 +0100

    Servo test rework

--- src/X-MRA/dpad_cmds_12servos.cpp
+++ src/X-MRA/dpad_cmds_12servos.cpp
@@ -3,49 +3,81 @@
 static void _begin();
 dPadBind bS12 = { [KEY_UNKNOWN] = _begin };
 
-#include "common_tools.h"
+// #include "common_tools.h"
 
 #include "X-MRA.hpp"
-#if 0
-#   define servoNum DIMOF(Servo)
-#else
-#   define servoNum (6*3)
-// #   define servoNum (2*3)
-#endif
+#include "WS2812_Spi.hpp"
+#define WS_LED_COUNT (7)
+Ws2812Spi RING7(WS_LED_COUNT);
+#define servoNum (6*3)
 
-#if 0
-#include "PS_MG996R.hpp"    // MG996R
-extern MG996R Servo[];
-#else
 #include "PS_FreeNove.hpp"  // sFreeNove
 extern sFreeNove Servo[];
-#endif
 
-static float _curAngle = 0.f;
-void allSv(float ang) {
+uint8_t lPatt = 0b111111;
+uint8_t jPatt = 0x07;
+inline bool isActive(int i) {
+     return
+        (lPatt & (1 << (i / 3))) &&
+        (jPatt & (1 << (i % 3)));
+}
+
+int map[7] = {2, 4, 1, 5, 0, 6, 3};
+bool isAll = false;
+static float _curAngle = 2.f;
+void allSv(float ang, float dAng = 0.f) {
     LOG("\r[%.2f]    ", (_curAngle = ang));
     for(int idxSv = 0; idxSv < servoNum; idxSv++)
-        Servo[idxSv].setAngle(deg(ang));
-}
-static void _left()  { allSv(_curAngle - 1); }
-static void _right() { allSv(_curAngle + 1); }
+        if (isAll ||
+            (isActive(idxSv))
+            )   if (dAng != 0.f) {
+                    Servo[idxSv].setAngle(
+                        Servo[idxSv].getAngle() + deg(dAng)
+                    );
+                } else {
+                    Servo[idxSv].setAngle(deg(ang));
+                }
 
-static void _1()  { allSv(  0.f); } static void __1()  { allSv(  0.f); }
-static void _2()  { allSv(-10.f); } static void __2()  { allSv( 10.f); }
-static void _3()  { allSv(-20.f); } static void __3()  { allSv( 20.f); }
-static void _4()  { allSv(-30.f); } static void __4()  { allSv( 30.f); }
-static void _5()  { allSv(-40.f); } static void __5()  { allSv( 40.f); }
-static void _6()  { allSv(-50.f); } static void __6()  { allSv( 50.f); }
-static void _7()  { allSv(-60.f); } static void __7()  { allSv( 60.f); }
-static void _8()  { allSv(-70.f); } static void __8()  { allSv( 70.f); }
-static void _9()  { allSv(-80.f); } static void __9()  { allSv( 80.f); }
-static void _10() { allSv(-90.f); } static void __10() { allSv( 90.f); }
+    for (int i = 0; i < RING7.getNumLeds(); i++) {
+        RING7.setPixel(
+            map[i],
+            RGB(
+                ((jPatt & (1 << Coxa)) != 0),
+                ((jPatt & (1 << Femur)) != 0),
+                ((jPatt & (1 << Tibia)) != 0)
+            ) * ((lPatt & (1 << i)) != 0)
+        );
+    }
+    RING7.show();
+}
+
+static void _left()  { allSv(0.f, -_curAngle); }
+static void _right() { allSv(0.f, _curAngle); }
+
+static void  _1()   { lPatt = (1 << 0); allSv(0); }
+static void  _2()   { lPatt = (1 << 1); allSv(0); }
+static void  _3()   { lPatt = (1 << 2); allSv(0); }
+static void  _4()   { lPatt = (1 << 3); allSv(0); }
+static void  _5()   { lPatt = (1 << 4); allSv(0); }
+static void  _6()   { lPatt = (1 << 5); allSv(0); }
+
+static void __1()   { lPatt ^= (1 << 0); allSv(0); }
+static void __2()   { lPatt ^= (1 << 1); allSv(0); }
+static void __3()   { lPatt ^= (1 << 2); allSv(0); }
+static void __4()   { lPatt ^= (1 << 3); allSv(0); }
+static void __5()   { lPatt ^= (1 << 4); allSv(0); }
+static void __6()   { lPatt ^= (1 << 5); allSv(0); }
+
+static void __7()   { jPatt ^= (1 << 0); allSv(0); }
+static void __8()   { jPatt ^= (1 << 1); allSv(0); }
+static void __9()   { jPatt ^= (1 << 2); allSv(0); }
+
 
 static void _on()  { LOG("ON\n");  PowerAllow(true);  }
 static void _off() { LOG("OFF\n"); PowerAllow(false); }
 
 static void _1on()  { LOG("1 ON\n");  Servo[1].enable();  }
-static void _1off() { LOG("2 OFF\n"); Servo[1].disable(); }
+static void _1off() { LOG("1 OFF\n"); Servo[1].disable(); }
 
 static void _2on()  { LOG("2 ON\n");  Servo[2].enable();  }
 static void _2off() { LOG("2 OFF\n"); Servo[2].disable(); }
@@ -59,7 +91,7 @@ static void _begin() {
         "q/Q        - On/Off(all)\n"
         "w/W        - On/Off(1)\n"
         "e/D        - On/Off(2)\n"
-        "0..9 +/-Sh - setAng(0..9)\n"
+        // "0..9 +/-Sh - setAng(0..9)\n"
         "</>        - +/- Angqel\n"
         "A          - readADC\n"
     );
@@ -75,10 +107,12 @@ static void _begin() {
     bS12[KEY_4] = _4;    bS12[KEY__4] = __4;
     bS12[KEY_5] = _5;    bS12[KEY__5] = __5;
     bS12[KEY_6] = _6;    bS12[KEY__6] = __6;
-    // bS12[KEY_7] = _7;    bS12[KEY__7] = __7;
-    // bS12[KEY_8] = _8;    bS12[KEY__8] = __8;
-    // bS12[KEY_9] = _9;    bS12[KEY__9] = __9;
-    // bS12[KEY_0] = _10;   bS12[KEY__0] = __10;
+
+    bS12[KEY__7] = __7;
+    bS12[KEY__8] = __8;
+    bS12[KEY__9] = __9;
+
+    // bS12[KEY_0] = _0;    bS12[KEY__0] = __0;
 
     bS12[KEY__Q] = _on;  bS12[KEY_Q] = _off;
     bS12[KEY__W] = _1on; bS12[KEY_W] = _1off;
